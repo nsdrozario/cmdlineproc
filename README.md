062b21a6a401c3a6c5fa1a121a7c72dcbbcabb64
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-07 16:31:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# cmdlineproc
+
+A header-only, lightweight library for processing command line arguments and options/flags using only C++ STL structures and methods.
+
+## How to use
+
+First, make sure you Include `cmdlineproc.hpp` in your project's include search paths.
+Then, for example, we can do the following code, which outputs all arguments of the flag `-b`:
+
+```
+#include <iostream>
+#include <cmdlineproc.hpp>
+
+cmdlineproc::cmd_args c; // initialize the cmd_args class
+
+int main (int argc, char *argv[]) {
+
+    c.set_flag("-h"); // register the -h option, requiring 0 arguments
+    c.set_flag("-a", 1); // register the -a option, which will require one argument after the flag (for example, [program] -a [arg])  
+    c.set_flag("-b", 2); // same as -a but with two arguments (e.g. [program] -b [arg1] [arg2])
+
+    auto it_range = c.flag_args.equal_range("-b"); // get an iterator pair with all arguments of flag "-b"
+    
+    c.init_args(argc, argv); // load command line arguments
+
+    for (auto it1 = it_range.first; it1 < it_range.second; it++) {
+        std::cout << *it1 << std::endl; // print all arguments of flag "-b"
+    }
+
+    return 0;
+
+}
+
+```
+
+When running this program with the arguments `-b string1 string2`, the output should be:
+
+```
+string1
+string2
+```
+
+Regular arguments that are passed to the program, not related to any flag/option, can be found in the vector `cmd_args.args`.
+Flags/options that are passed to the program that require 0 arguments can be found in the vector `cmd_args.flags`.
+Any flags that aren't registered with `cmd_args.set_flag` will be ignored.
+
+## License
+
+This library is under the MIT License.
---
+++ include/cmdlineproc.hpp
@@ -0,0 +1,97 @@
+/*
+
+cmdlineproc.hpp (c) 2020 Nathaniel D'Rozario
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#ifndef CMDLINEPROC_HPP
+#define CMDLINEPROC_HPP
+
+#include <string>
+#include <map>
+#include <vector>
+
+namespace cmdlineproc {
+
+    class cmd_args {
+
+        public:
+
+            std::multimap<std::string, std::string> flag_args; // multimap with arguments for flags that have args > 0
+            std::vector<std::string> flags; // flags from argv[] that have args == 0
+            std::vector<std::string> args; // regular arguments
+
+            cmd_args();
+
+            void set_flag(std::string s, int args=0) { // args is how many arguments the flag takes, default=0
+
+                flag_arg_count.insert(std::make_pair(s,args));
+
+            } 
+
+            void cmd_args::init_args(int argc, char *argv[]) { // takes argc and argv from int main() and processes them
+
+                for (int i = 1; i < argc; i++) {
+                    
+                    std::string current_arg (argv[i]);
+
+                    if (current_arg[0] == '-') { 
+                        
+                        if (flag_arg_count.find(current_arg) != flag_arg_count.end()) {
+
+                            if (flag_arg_count[current_arg] == 0) {
+
+                                flags.push_back(current_arg);
+
+                            } else {
+                                
+                                int current_i = i;
+                                i++;
+                                
+                                while (i < current_i+flag_arg_count[current_arg] && i < argc) {
+
+                                    std::string flag_arg (argv[i]);
+                                    flag_args.insert(make_pair(current_arg, flag_arg));
+                                    i++;
+
+                                }
+
+                            }
+
+                        } // ignore unknown flags
+
+                    } else {
+                        args.push_back(current_arg);
+                    }
+                
+                }
+
+            } 
+
+        private:
+
+            std::map<std::string, int> flag_arg_count;
+
+    };
+
+}
+
+#endif
